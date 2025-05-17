ee4d0aad71ba5a0d0ef9ce46ac078bce01da4245
Patrick Stein <patrick.stein@jinx.de>
2025-05-17 06:36:33 +0200

    updated packaged added test.

--- Package.swift
+++ Package.swift
@@ -6,10 +6,8 @@ import PackageDescription
 let package = Package(
     name: "modbus2mqtt",
     platforms: [
-        .iOS(.v16),
-        .macOS(.v13),
-        .tvOS(.v13),
-        .watchOS(.v6)
+        .iOS(.v18),
+        .macOS(.v15)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
@@ -20,7 +18,7 @@ let package = Package(
         .package(url: "https://github.com/nicklockwood/SwiftFormat", branch: "main"),
         .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMajor(from: "1.2.2")),
         .package(url: "https://github.com/sroebert/mqtt-nio.git", from: "2.8.0"),
-        .package(url: "https://github.com/jollyjinx/JLog", .upToNextMajor(from: "0.0.5")),
+        .package(url: "https://github.com/jollyjinx/JLog", .upToNextMajor(from: "0.0.7")),
         .package(url: "https://github.com/jollyjinx/SwiftLibModbus", from:"2.0.2"),
     ],
     targets: [
--- Tests/modbus2mqttTests/modbus2mqttTests.swift
+++ Tests/modbus2mqttTests/modbus2mqttTests.swift
@@ -6,80 +6,11 @@ import Foundation
 import JLog
 import SwiftLibModbus
 import Testing
-
-@testable import SwiftLibModbus2MQTT
-
-public extension Encodable
-{
-    var json: String
-    {
-        let jsonEncoder = JSONEncoder()
-        jsonEncoder.outputFormatting = [.sortedKeys]
-        jsonEncoder.dateEncodingStrategy = .iso8601
-        jsonEncoder.outputFormatting = [.prettyPrinted]
-        let jsonData = try? jsonEncoder.encode(self)
-        return jsonData != nil ? String(data: jsonData!, encoding: .utf8) ?? "" : ""
-    }
-}
-
-public extension Decodable
-{
-    init(json: String) throws
-    {
-        print("Decodable:\(json)")
-        print("Self:\(Self.self)")
-        let decoder = JSONDecoder()
-        decoder.dateDecodingStrategy = .iso8601
-        let data = json.data(using: .utf8)!
-        self = try decoder.decode(Self.self, from: data)
-    }
-}
+import SwiftLibModbus2MQTT
 
 @Suite("Modbus Tests")
 struct modbus2mqttTests
 {
-    @Test(.disabled("Only works when attached"))
-    func reverseEngineerHM310T() async throws
-    {
-        // Prints out modbus address ranges and compares them to the last time
-
-        let modbusDevice = try ModbusDevice(device: "/dev/tty.usbserial-42340", baudRate: 9600)
-        let stripesize = 0x10
-
-        var store = [Int: [UInt16]]()
-        let emptyline = [UInt16](repeating: 0, count: stripesize)
-
-        func readData(from address: Int) async throws
-        {
-            let data: [UInt16] = try await modbusDevice.readRegisters(from: address, count: stripesize, type: .holding)
-
-            let previous: [UInt16] = store[address] ?? emptyline
-
-            if data != previous
-            {
-                print("\(String(format: "%04x", address)): \(data.map { $0 == 0 ? "  -   " : String(format: "%04x  ", $0) }.joined(separator: " ")) ")
-                print("\(String(format: "%04x", address)): \(data.map { $0 == 0 ? "      " : String(format: "%05d ", $0) }.joined(separator: " ")) ")
-                print("")
-                store[address] = data
-            }
-        }
-
-        for address in stride(from: 0x000, to: 0xFFFF, by: stripesize)
-        {
-            try await readData(from: address)
-        }
-
-        for _ in 0 ... 20
-        {
-            print("WRAPAROUND")
-
-            for address in store.keys
-            {
-                try await readData(from: address)
-            }
-        }
-    }
-
     @Test
     func BrokenJSONDefinition() async throws
     {
