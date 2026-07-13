48317101a719572c080d252977f829af50e17c94
Patrick Stein <patrick.stein@jinx.de>
2026-07-13 21:44:13 +0200

    Adopt SwiftLibModbus 3.0.0

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(name: "modbus2mqtt",
                           .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMajor(from: "1.6.2")),
                           .package(url: "https://github.com/sroebert/mqtt-nio.git", from: "2.8.0"),
                           .package(url: "https://github.com/jollyjinx/JLog", .upToNextMajor(from: "0.0.9")),
-                          .package(url: "https://github.com/jollyjinx/SwiftLibModbus", revision: "5da5e53d39c7f9eeda066ac08964613237572efc"),
+                          .package(url: "https://github.com/jollyjinx/SwiftLibModbus", .upToNextMajor(from: "3.0.0")),
                       ],
                       targets: [
                           .executableTarget(name: "modbus2mqtt",
